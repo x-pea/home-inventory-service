39d26688ae38c9d40befe595a71fcf775d0e2298
Ephraim Glick <ephraim.glick@gmail.com>
2017-12-08 21:02:47 -0800

    Set up basic Express server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const createSQL = require('../database/createSQL');
+const seedSQL = require('../database/seedSQL');
+
+// require('../database/seedSQL');
+const express = require('express');
+
+createSQL.init();
+seedSQL.init();
+
+const app = express();
+app.get('/', (req, res) => res.send('Welcome to the home inventory service'));
+
+app.listen(process.env.PORT, () => console.log(`Express server is listening on port ${process.env.PORT}`));
