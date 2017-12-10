71b991668e9d4caac1ed496df12c128a34c659e6
Ephraim Glick <ephraim.glick@gmail.com>
2017-12-09 18:23:37 -0800

    Three tests for SQS

--- test/spec.js
+++ test/spec.js
@@ -1,9 +1,14 @@
+/* eslint-disable */
+
 require('dotenv').config();
+const Promise = require('bluebird');
 const mysql = require('mysql');
 const expect = require('chai').expect;
 const createSQL = require('../database/createSQL.js');
 const seedSQL = require('../database/seedSQL.js');
 const server = require('../server/index.js');
+// const AWS = require('aws-sdk');
+const setupSQS = require('../server/setupSQS.js');
 const supertest = require('supertest');
 const testRequest = supertest.agent(server);
 
@@ -30,9 +35,84 @@ describe('** Node / Express server **', function() {
       testRequest
         .get('/')
         .expect(200)
-        .then(() => done())
+        .then(() => done());
     });
   });
 
+}); // end tests for Node / Express server
 
-});
+describe('** AWS SQS **', function() {
+
+  var receiptHandleForDeleteTest;
+
+  describe('When sending a message', function () {
+    it('should respond with an object containing the message ID', function (done) {
+      const sendingParams = {
+        DelaySeconds: 10,
+        MessageAttributes: {
+          Greeting: {
+            DataType: 'String',
+            StringValue: 'Hello, Tester!'
+          }
+        },
+        MessageBody: 'A sample message to test SQS',
+        QueueUrl: process.env.SQS_QUEUE_URL
+      };
+      setupSQS.sqs.sendMessage(sendingParams)
+        .then(function(data) {
+          expect(data.MessageId).to.be.a('string');
+          done();
+        })
+        .catch(function(err) {
+          console.log('Error', err)
+        });
+    });
+  });
+
+  describe('When receiving messages', function () {
+
+    it('should respond with an object containing the message ID', function (done) {
+      const receivingParams = {
+        QueueUrl: process.env.SQS_QUEUE_URL,
+        MaxNumberOfMessages: 10,
+        VisibilityTimeout: 0,
+        WaitTimeSeconds: 1
+      };
+      setupSQS.sqs.receiveMessage(receivingParams)
+        .then(function(data) {
+          console.log(Object.keys(data));
+          expect(data.Messages).to.be.an('array');
+          expect(data.Messages[0]).to.be.an('object');
+          expect(data.Messages[0]).to.have.property('ReceiptHandle');
+          receiptHandleForDeleteTest = data.Messages[0].ReceiptHandle;
+          done();
+        })
+        .catch(function(err) {
+          console.log(err, err.stack)
+        });
+    });
+  });
+
+  describe('When deleting a message', function () {
+    it('should respond with an object containing the message ID', function (done) {
+      const deleteParams = {
+        QueueUrl: process.env.SQS_QUEUE_URL,
+        ReceiptHandle: receiptHandleForDeleteTest
+      };
+      setupSQS.sqs.deleteMessage(deleteParams)
+        .then(function(data) {
+          expect(data.ResponseMetadata).to.be.an('object').with.property('RequestId');
+          done();
+        })
+        .catch(function(err) {
+          console.log(err, err.stack)
+        });
+    });
+  });
+
+}); // end tests for SQS 
+
+
+
+
+
