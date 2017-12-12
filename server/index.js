655f7fa6052eb59e0ac55fd27248440e7b819ccd
Ephraim Glick <ephraim.glick@gmail.com>
2017-12-11 19:22:51 -0800

    ES6-ify

--- server/index.js
+++ server/index.js
@@ -1,18 +1,19 @@
-require('dotenv').config();
-const createSQL = require('../database/createSQL');
-const seedSQL = require('../database/seedSQL');
-// const sqs = require('./setupSQS');
-
-const express = require('express');
+import {} from 'dotenv/config';
+import express from 'express';
+import * as createSQL from '../database/createSQL';
+import * as seedSQL from '../database/seedSQL';
+import redis from '../database/redis.js';
+// import redis from 'redis';
 
 createSQL.init();
 seedSQL.init();
 
 const app = express();
+
 app.get('/', (req, res) => res.send('Welcome to the home inventory service'));
 
-if (module.parent) {
-  module.exports = app; // this is for test suite
-} else {
+if (!module.parent) {
   app.listen(process.env.PORT, () => console.log(`Express server is listening on port ${process.env.PORT}`));
 }
+
+export default app; // this is for test suite
