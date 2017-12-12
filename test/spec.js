e725bfd5d5c9a234622aa97215a03e706fb66786
Ephraim Glick <ephraim.glick@gmail.com>
2017-12-11 19:18:05 -0800

    Add tests for Redis

--- test/spec.js
+++ test/spec.js
@@ -1,15 +1,18 @@
 /* eslint-disable */
 
-require('dotenv').config();
-const Promise = require('bluebird');
-const mysql = require('mysql');
-const expect = require('chai').expect;
-const createSQL = require('../database/createSQL.js');
-const seedSQL = require('../database/seedSQL.js');
-const server = require('../server/index.js');
-// const AWS = require('aws-sdk');
-const setupSQS = require('../server/setupSQS.js');
-const supertest = require('supertest');
+import { config } from 'dotenv';
+import Promise from 'bluebird';
+import * as mysql from 'mysql';
+import redis from 'redis';
+import client from '../database/redis.js';
+import { expect } from 'chai';
+import * as createSQL from '../database/createSQL';
+import * as seedSQL from '../database/seedSQL';
+import server from '../server/index.js';
+import AWS from 'aws-sdk';
+import sqs from '../server/setupSQS.js';
+import supertest from 'supertest';
+
 const testRequest = supertest.agent(server);
 
 describe('** Node / Express server **', function() {
@@ -41,14 +44,15 @@ describe('** Node / Express server **', function() {
 
 }); // end tests for Node / Express server
 
-describe('** AWS SQS **', function() {
+xdescribe('** AWS SQS **', function() {
 
+  // Use this for the delete test later
   var receiptHandleForDeleteTest;
 
   describe('When sending a message', function () {
     it('should respond with an object containing the message ID', function (done) {
       const sendingParams = {
-        DelaySeconds: 10,
+        DelaySeconds: 0,
         MessageAttributes: {
           Greeting: {
             DataType: 'String',
@@ -56,15 +60,15 @@ describe('** AWS SQS **', function() {
           }
         },
         MessageBody: 'A sample message to test SQS',
-        QueueUrl: process.env.SQS_QUEUE_URL
+        QueueUrl: process.env.SQS_TESTQUEUE_URL
       };
-      setupSQS.sqs.sendMessage(sendingParams)
+      sqs.sendMessage(sendingParams)
         .then(function(data) {
           expect(data.MessageId).to.be.a('string');
           done();
         })
         .catch(function(err) {
-          console.log('Error', err)
+          done(err);
         });
     });
   });
@@ -73,14 +77,14 @@ describe('** AWS SQS **', function() {
 
     it('should respond with an object containing the message ID', function (done) {
       const receivingParams = {
-        QueueUrl: process.env.SQS_QUEUE_URL,
+        QueueUrl: process.env.SQS_TESTQUEUE_URL,
         MaxNumberOfMessages: 10,
-        VisibilityTimeout: 0,
-        WaitTimeSeconds: 1
+        // VisibilityTimeout: 10, // set in GUI!
+        // WaitTimeSeconds: 1 // set in GUI!
       };
-      setupSQS.sqs.receiveMessage(receivingParams)
+      sqs.receiveMessage(receivingParams)
         .then(function(data) {
-          console.log(Object.keys(data));
+          console.log(data);
           expect(data.Messages).to.be.an('array');
           expect(data.Messages[0]).to.be.an('object');
           expect(data.Messages[0]).to.have.property('ReceiptHandle');
@@ -96,10 +100,10 @@ describe('** AWS SQS **', function() {
   describe('When deleting a message', function () {
     it('should respond with an object containing the message ID', function (done) {
       const deleteParams = {
-        QueueUrl: process.env.SQS_QUEUE_URL,
+        QueueUrl: process.env.SQS_TESTQUEUE_URL,
         ReceiptHandle: receiptHandleForDeleteTest
       };
-      setupSQS.sqs.deleteMessage(deleteParams)
+      sqs.deleteMessage(deleteParams)
         .then(function(data) {
           expect(data.ResponseMetadata).to.be.an('object').with.property('RequestId');
           done();
@@ -112,7 +116,75 @@ describe('** AWS SQS **', function() {
 
 }); // end tests for SQS 
 
+// Redis must already be running for this to work
+describe('** Redis database **', function() {
 
+  describe('The set method', function () {
+    it('should set a value for a key', function (done) {
+      client.setAsync('potato', 'trump')
+        .then(reply => {
+          console.log(reply.toString());
+          expect(reply).to.equal('OK');
+          done();
+        })
+        .catch(err => {
+          console.log('Error setting value for a key: ', err);
+          done();
+        });
+    });
+  });
+
+  describe('The get method', function () {
+    it('should get the value of a key', function (done) {
+      client.getAsync('potato')
+        .then(reply => {
+          expect(reply).to.equal('trump');
+          done();
+        })
+        .catch(err => {
+          console.log('Error getting value for a key: ', err);
+          done();
+        });
+    });
+  });
+
+  describe('The del (delete) method', function () {
+    it('should delete a key and value from redis', function (done) {
+      client.delAsync('potato')
+        .then(() => client.getAsync('potato'))
+        .then(reply => {
+          console.log(reply);
+          expect(reply).to.equal(null);
+          done();
+        })
+        .catch(err => {
+          console.log('Error deleting from Redis: ', err);
+          done();
+        });
+    });
+  });
+  
+  describe('The hmset method', function () {
+    it('should use a hash to store key / value pairs', function (done) {
+      client.hmsetAsync('myhash', 'username', 'trump', 'password', 'breitbart')
+        .then(reply => {
+          expect(reply).to.equal('OK');
+          return client.hmgetAsync('myhash', 'password', 'username')
+        })
+        .then(reply => {
+          expect(reply).to.eql(['breitbart', 'trump']);
+          client.quit();
+          done();
+        })     
+        .catch(err => {
+          console.log('Error using hmset with Redis: ', err);
+          client.quit();
+          done();
+        });
+    });
+  });
+
+}); // end tests for Redis database
 
 
 
