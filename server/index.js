422bb3d1a68174611d828ac4afedb46dafd51b3c
Ephraim Glick <ephraim.glick@gmail.com>
2017-12-13 12:22:32 -0800

    Add /homes POST route for hosts

--- server/index.js
+++ server/index.js
@@ -1,16 +1,25 @@
 import {} from 'dotenv/config';
 import express from 'express';
+import bodyParser from 'body-parser';
 import * as createSQL from '../database/createSQL';
 import seedSQL from '../database/seedSQL';
-import redis from '../database/redis.js';
+import * as dbHelpers from '../database/dbHelpers';
+// import redis from '../database/redis.js';
 // import redis from 'redis';
 
 createSQL.init()
-  // .then(() => seedSQL());
+  .then(() => seedSQL());
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Welcome to the home inventory service'));
+
+app.post('/homes', (req, res) => {
+  dbHelpers.saveNewHomes(req.body)
+    .then(() => res.status(201).send())
+    .catch(err => res.status(500).send(err));
+});
 
 if (!module.parent) {
   app.listen(process.env.PORT, () => console.log(`Express server is listening on port ${process.env.PORT}`));
