6e9723a6d6a630c32ff7b84fdd2eef6b9cdb2303
Ephraim Glick <ephraim.glick@gmail.com>
2017-12-13 16:29:07 -0800

    POST and PATCH to /homes saves and send sqs

--- server/index.js
+++ server/index.js
@@ -3,7 +3,9 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import * as createSQL from '../database/createSQL';
 import seedSQL from '../database/seedSQL';
-import * as dbHelpers from '../database/dbHelpers';
+import sqs from '../server/setupSQS.js';
+
+// import * as dbHelpers from '../database/dbHelpers';
 // import redis from '../database/redis.js';
 // import redis from 'redis';
 
@@ -16,8 +18,21 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => res.send('Welcome to the home inventory service'));
 
 app.post('/homes', (req, res) => {
-  dbHelpers.saveNewHomes(req.body)
-    .then(() => res.status(201).send())
+  // Note, this object syntax isn't supported for batch inserting!
+  createSQL.connection.queryAsync('INSERT INTO homes SET ?', [req.body])
+    .tap(() => sqs.sendToClient(JSON.stringify({ newPost: req.body })))
+    .tap(() => sqs.sendToReservations(JSON.stringify({ newPost: req.body })))
+    .then(() => res.status(201).send(req.body))
+    .catch(err => res.status(500).send(err));
+});
+
+app.patch('/homes', (req, res) => {
+  const updateData = Object.assign({}, req.body);
+  delete updateData.id;
+  createSQL.connection.queryAsync('UPDATE homes SET ? WHERE id = ?', [updateData, req.body.id])
+    .tap(() => sqs.sendToClient(JSON.stringify({ update: req.body })))
+    .tap(() => sqs.sendToReservations(JSON.stringify({ update: req.body })))
+    .then(() => res.status(201).send(req.body))
     .catch(err => res.status(500).send(err));
 });
 
