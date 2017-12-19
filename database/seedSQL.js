995f3a87f5e4560f358abb2db495401a94fd24cb
Ephraim Glick <ephraim.glick@gmail.com>
2017-12-18 17:44:50 -0800

    Disconnect from server

--- database/seedSQL.js
+++ database/seedSQL.js
@@ -1,16 +1,23 @@
-import faker from 'faker';
-import * as createSQL from './createSQL';
+// Run this file on its own before starting the app
 
-// Number of homes and rooms to create
-const fakeHomesCount = 1000;
-const fakeRoomsCount = 333;
+import { Promise } from 'bluebird';
+import faker from 'faker';
+import connection from './index';
+
+// Number of entries to create
+const fakeHostsCount = 9000;
+const fakeHomesCount = 10000;
+const fakeRoomsCount = 3333;
+const fakeReservationsCount = 13333;
+
 // Hard-coded sample cities, length 387
 const cities = ['Aberdeen', 'Abilene', 'Akron', 'Albany', 'Albuquerque', 'Alexandria', 'Allentown', 'Amarillo', 'Anaheim', 'Anchorage', 'Ann Arbor', 'Antioch', 'Apple Valley', 'Appleton', 'Arlington', 'Arvada', 'Asheville', 'Athens', 'Atlanta', 'Atlantic City', 'Augusta', 'Aurora', 'Austin', 'Bakersfield', 'Baltimore', 'Barnstable', 'Baton Rouge', 'Beaumont', 'Bel Air', 'Bellevue', 'Berkeley', 'Bethlehem', 'Billings', 'Birmingham', 'Bloomington', 'Boise', 'Boise City', 'Bonita Springs', 'Boston', 'Boulder', 'Bradenton', 'Bremerton', 'Bridgeport', 'Brighton', 'Brownsville', 'Bryan', 'Buffalo', 'Burbank', 'Burlington', 'Buttsville', 'Cambridge', 'Canton', 'Cape Coral', 'Carrollton', 'Cary', 'Cathedral City', 'Cedar Rapids', 'Champaign', 'Chandler', 'Charleston', 'Charlotte', 'Chattanooga', 'Chesapeake', 'Chicago', 'Chula Vista', 'Cincinnati', 'Clarke County', 'Clarksville', 'Clearwater', 'Cleveland', 'College Station', 'Colorado Springs', 'Columbia', 'Columbus', 'Concord', 'Coral Springs', 'Corona', 'Corpus Christi', 'Costa Mesa', 'Dallas', 'Daly City', 'Danbury', 'Davenport', 'Davidson County', 'Dayton', 'Daytona Beach', 'Deltona', 'Denton', 'Denver', 'Des Moines', 'Detroit', 'Downey', 'Duluth', 'Durham', 'El Monte', 'El Paso', 'Elizabeth', 'Elk Grove', 'Elkhart', 'Erie', 'Escondido', 'Eugene', 'Evansville', 'Fairfield', 'Fargo', 'Fartstoon', 'Fayetteville', 'Fitchburg', 'Flint', 'Fontana', 'Fort Collins', 'Fort Lauderdale', 'Fort Smith', 'Fort Walton Beach', 'Fort Wayne', 'Fort Worth', 'Frederick', 'Fremont', 'Fresno', 'Fullerton', 'Gainesville', 'Garden Grove', 'Garland', 'Gastonia', 'Gilbert', 'Glendale', 'Grand Prairie', 'Grand Rapids', 'Grayslake', 'Green Bay', 'GreenBay', 'Greensboro', 'Greenville', 'Gulfport-Biloxi', 'Hagerstown', 'Hampton', 'Harlingen', 'Harrisburg', 'Hartford', 'Havre de Grace', 'Hayward', 'Hemet', 'Henderson', 'Hesperia', 'Hialeah', 'Hickory', 'High Point', 'Hollywood', 'Honolulu', 'Houma', 'Houston', 'Howell', 'Huntington', 'Huntington Beach', 'Huntsville', 'Independence', 'Indianapolis', 'Inglewood', 'Irvine', 'Irving', 'Jackson', 'Jacksonville', 'Jefferson', 'Jersey City', 'Johnson City', 'Joliet', 'Kailua', 'Kalamazoo', 'Kaneohe', 'Kansas City', 'Kennewick', 'Kenosha', 'Killeen', 'Kissimmee', 'Knoxville', 'Lacey', 'Lafayette', 'Lake Charles', 'Lakeland', 'Lakewood', 'Lancaster', 'Lansing', 'Laredo', 'Las Cruces', 'Las Vegas', 'Layton', 'Leominster', 'Lewisville', 'Lexington', 'Lincoln', 'Little Rock', 'Long Beach', 'Lorain', 'Los Angeles', 'Louisville', 'Lowell', 'Lubbock', 'Macon', 'Madison', 'Manchester', 'Marina', 'Marysville', 'McAllen', 'McHenry', 'Medford', 'Melbourne', 'Memphis', 'Merced', 'Mesa', 'Mesquite', 'Miami', 'Milwaukee', 'Minneapolis', 'Miramar', 'Mission Viejo', 'Mobile', 'Modesto', 'Monroe', 'Monterey', 'Montgomery', 'Moreno Valley', 'Murfreesboro', 'Murrieta', 'Muskegon', 'Myrtle Beach', 'Naperville', 'Naples', 'Nashua', 'Nashville', 'New Bedford', 'New Haven', 'New London', 'New Orleans', 'New York', 'New York City', 'Newark', 'Newburgh', 'Newport News', 'Norfolk', 'Normal', 'Norman', 'North Charleston', 'North Las Vegas', 'North Port', 'Norwalk', 'Norwich', 'Oakland', 'Ocala', 'Oceanside', 'Odessa', 'Ogden', 'Oklahoma City', 'Olathe', 'Olympia', 'Omaha', 'Ontario', 'Orange', 'Orem', 'Orlando', 'Overland Park', 'Oxnard', 'Palm Bay', 'Palm Springs', 'Palmdale', 'Panama City', 'Pasadena', 'Paterson', 'Pembroke Pines', 'Pensacola', 'Peoria', 'Philadelphia', 'Phoenix', 'Pittsburgh', 'Plano', 'Pomona', 'Pompano Beach', 'Port Arthur', 'Port Orange', 'Port Saint Lucie', 'Port St. Lucie', 'Portland', 'Portsmouth', 'Poughkeepsie', 'Providence', 'Provo', 'Pueblo', 'Punta Gorda', 'Racine', 'Raleigh', 'Rancho Cucamonga', 'Reading', 'Redding', 'Reno', 'Richland', 'Richmond', 'Richmond County', 'Riverside', 'Roanoke', 'Rochester', 'Rockford', 'Roseville', 'Round Lake Beach', 'Sacramento', 'Saginaw', 'Saint Louis', 'Saint Paul', 'Saint Petersburg', 'Salem', 'Salinas', 'Salt Lake City', 'San Antonio', 'San Bernardino', 'San Buenaventura', 'San Diego', 'San Francisco', 'San Jose', 'Santa Ana', 'Santa Barbara', 'Santa Clara', 'Santa Clarita', 'Santa Cruz', 'Santa Maria', 'Santa Rosa', 'Sarasota', 'Savannah', 'Scottsdale', 'Scranton', 'Seaside', 'Seattle', 'Sebastian', 'Shreveport', 'Simi Valley', 'Sioux City', 'Sioux Falls', 'South Bend', 'South Lyon', 'Spartanburg', 'Spokane', 'Springdale', 'Springfield', 'St. Louis', 'St. Paul', 'St. Petersburg', 'Stamford', 'Sterling Heights', 'Stockton', 'Sunnyvale', 'Syracuse', 'Tacoma', 'Tallahassee', 'Tampa', 'Temecula', 'Tempe', 'Thornton', 'Thousand Oaks', 'Toledo', 'Topeka', 'Torrance', 'Trenton', 'Tucson', 'Tulsa', 'Tuscaloosa', 'Tyler', 'Utica', 'Vallejo', 'Vancouver', 'Vero Beach', 'Victorville', 'Virginia Beach', 'Visalia', 'Waco', 'Warren', 'Washington', 'Waterbury', 'Waterloo', 'West Covina', 'West Valley City', 'Westminster', 'Wichita', 'Wilmington', 'Winston', 'Winter Haven', 'Worcester', 'Yakima', 'Yonkers', 'York', 'Youngstown'];
 // Hard-coded fake neighborhoods, use the same for each city
 const neighborhoods = ['Downtown', 'Uptown', 'East Side', 'West Side'];
-// Fake hosts
+
+// Save all fake hosts
 const fakeHosts = [];
-for (let i = 0; i < 900; i += 1) {
+for (let i = 0; i < fakeHostsCount; i += 1) {
   fakeHosts.push([faker.name.firstName(), faker.name.lastName()]);
 }
 
@@ -24,11 +31,11 @@ const getPrice = () => Math.floor(40 + (Math.random() * 150));
 
 // Save all the cities to mySQL
 const insertAllCities = () => (
-  createSQL.connection.queryAsync('SELECT id FROM cities ORDER BY id ASC LIMIT 1')
+  connection.queryAsync('SELECT id FROM cities ORDER BY id ASC LIMIT 1')
     .then(firstRow => {
       if (firstRow.id) { return firstRow; }
       const cityArrs = cities.map(city => [city]);
-      return createSQL.connection.queryAsync('INSERT INTO cities (name) VALUES ?', [cityArrs]);
+      return connection.queryAsync('INSERT INTO cities (name) VALUES ?', [cityArrs]);
     })
     .then(res => Promise.resolve(res.insertId ? res.insertId : res.id))
     .catch(err => console.log(err))
@@ -39,7 +46,7 @@ message: '(Records: 387  Duplicates: 0  Warnings: 0', protocol41: true, changedR
 
 // Save the fake neighborhoods for each city
 const insertAllNeighborhoods = firstCityId => (
-  createSQL.connection.queryAsync('SELECT id FROM neighborhoods ORDER BY id ASC LIMIT 1')
+  connection.queryAsync('SELECT id FROM neighborhoods ORDER BY id ASC LIMIT 1')
     .then(firstRow => {
       // If there are results, get the first neighborhood id
       if (firstRow.id) { return firstRow; }
@@ -51,7 +58,7 @@ const insertAllNeighborhoods = firstCityId => (
           hoodsToInsert.push([i, neighborhoods[j]]);
         }
       } // This will yield a response with an insertId
-      return createSQL.connection.queryAsync('INSERT INTO neighborhoods ' +
+      return connection.queryAsync('INSERT INTO neighborhoods ' +
         '(id_cities, name) VALUES ?', [hoodsToInsert]);
     })
     .then(res => {
@@ -63,12 +70,12 @@ const insertAllNeighborhoods = firstCityId => (
 
 // Save all the fake hosts
 const insertAllHosts = ({ firstCityId, firstHoodId }) => (
-  createSQL.connection.queryAsync('SELECT id FROM hosts ORDER BY id ASC LIMIT 1')
+  connection.queryAsync('SELECT id FROM hosts ORDER BY id ASC LIMIT 1')
     .then(firstRow => {
       // If there are results, get the first host id
       if (firstRow.id) { return firstRow; }
       // Else if there are no results, save all the hosts
-      return createSQL.connection // This will yield a response with an insertId
+      return connection // This will yield a response with an insertId
         .queryAsync('INSERT INTO hosts (first_name, last_name) VALUES ?', [fakeHosts]);
     })
     .then(res => {
@@ -81,7 +88,7 @@ const insertAllHosts = ({ firstCityId, firstHoodId }) => (
 // Create the fake homes (rooms come later)
 const saveAllFakeHomes = ({ firstCityId, firstHoodId, firstHostId }) => {
   const fakeHomesToMakeRooms = [];
-  return createSQL.connection.queryAsync('SELECT id FROM homes ORDER BY id ASC LIMIT 1')
+  return connection.queryAsync('SELECT id FROM homes ORDER BY id ASC LIMIT 1')
     .then(firstRow => {
       // If there are results, get the first home id
       if (firstRow.id) { return firstRow; }
@@ -89,15 +96,18 @@ const saveAllFakeHomes = ({ firstCityId, firstHoodId, firstHostId }) => {
       const fakeHomes = [];
       let fakeHome; // create just a few for now
       for (let i = 0; i < fakeHomesCount; i += 1) {
-        fakeHome = [getNeighborhood(firstHoodId), getCityId(firstCityId), getAddress(),
-          Math.floor(Math.random() * 8), getPrice(), Math.round(Math.random()), faker.image.nightlife(),
-          1, 1, null, firstHostId + Math.floor(Math.random() * fakeHosts.length)];
+        fakeHome = [
+          getNeighborhood(firstHoodId), getCityId(firstCityId),
+          getAddress(), Math.floor(Math.random() * 8), getPrice(),
+          Math.round(Math.random()), faker.image.nightlife(), 1, 1, null,
+          firstHostId + Math.floor(Math.random() * fakeHosts.length)
+        ];
         fakeHomes.push(fakeHome);
         // Save the first ten to use to create single rooms for rent
         if (i < fakeRoomsCount) { fakeHomesToMakeRooms.push(fakeHome); }
       }
       // This will yield a response with an insertId
-      return createSQL.connection.queryAsync('INSERT INTO homes ' +
+      return connection.queryAsync('INSERT INTO homes ' +
         '(id_neighborhoods, id_cities, address, ' +
         'max_guests, price_usd, instant_book, photos, entire_home, ' +
         'private, parent_id, id_hosts) VALUES ?', [fakeHomes]);
@@ -123,7 +133,7 @@ const saveAllFakeRooms = ({ firstHomeId, fakeHomesToMakeRooms }) => {
     // Give the room a price lower than the entire home
     fakeRooms[i][5] = homes[i][5] * 0.3;
   }
-  return createSQL.connection // This will yield a response with an insertId
+  return connection // This will yield a response with an insertId
     .queryAsync('INSERT INTO homes (id_neighborhoods, id_cities, address, ' +
       'max_guests, price_usd, instant_book, photos, entire_home, ' +
       'private, parent_id, id_hosts) VALUES ?', [fakeRooms])
@@ -133,29 +143,29 @@ const saveAllFakeRooms = ({ firstHomeId, fakeHomesToMakeRooms }) => {
 // Create fake reservations (only track booked days for each home)
 const saveAllFakeReservations = firstHomeId => {
   const fakeReservations = [];
-  for (let i = 0; i < fakeHomesCount; i += 1) {
+  for (let i = 0; i < fakeReservationsCount; i += 1) {
     fakeReservations.push([
       faker.date.future(),
-      firstHomeId + Math.round(Math.random() *
-        (fakeHomesCount + fakeRoomsCount))
+      firstHomeId + Math.floor(Math.random() * fakeReservationsCount)
     ]);
   }
-  return createSQL.connection.queryAsync('INSERT INTO reservations ' +
+  return connection.queryAsync('INSERT INTO reservations ' +
     '(date, homes_id) VALUES ?', [fakeReservations]);
 };
 
-const init = () => (
-  insertAllCities()
-    .then(firstCityId => insertAllNeighborhoods(firstCityId))
-    // .tap(firstRows => console.log(firstRows))
-    .then(firstRows => insertAllHosts(firstRows))
-    // .tap(firstRows => console.log(firstRows))
-    .then(firstRows => saveAllFakeHomes(firstRows))
-    // .tap(firstRows => console.log(firstRows))
-    .then(firstRows => saveAllFakeRooms(firstRows))
-    .then(firstHomeId => saveAllFakeReservations(firstHomeId))
-    .tap(() => console.log('Success saving all fake data!'))
-    .catch(err => console.log(err))
-);
-
-export default init;
+// Main function to insert all data
+connection.pingAsync()
+  .catch(err => console.log('Error: No response to mySQL ping. ', err))
+  // create airbnb database
+  .then(() => connection.queryAsync('USE airbnb'))
+  .then(() => insertAllCities())
+  .then(firstCityId => insertAllNeighborhoods(firstCityId))
+  // .tap(firstRows => console.log(firstRows))
+  .then(firstRows => insertAllHosts(firstRows))
+  // .tap(firstRows => console.log(firstRows))
+  .then(firstRows => saveAllFakeHomes(firstRows))
+  // .tap(firstRows => console.log(firstRows))
+  .then(firstRows => saveAllFakeRooms(firstRows))
+  .then(firstHomeId => saveAllFakeReservations(firstHomeId))
+  .tap(() => console.log('All fake data has been saved successfully'))
+  .catch(err => console.log(err));
