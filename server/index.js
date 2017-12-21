765002f3f97ddb847c8652bdbf8a5dc4e4d80262
Ephraim Glick <ephraim.glick@gmail.com>
2017-12-20 20:07:35 -0800

    Uncomment sqs actions

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import {} from 'dotenv/config';
 import express from 'express';
 import bodyParser from 'body-parser';
 import sql from '../database/index';
+import sqs from './setupSQS';
 
 // import * as dbHelpers from '../database/dbHelpers';
 // import redis from '../database/redis.js';
@@ -21,8 +22,8 @@ app.get('/', (req, res) => res.send('Welcome to the home inventory service'));
 app.get('/homes', (req, res) => (
   // Note, this object syntax isn't supported for batch inserting!
   sql.queryAsync('SELECT * FROM homes WHERE id = ?', [req.body.id])
-    // .tap(() => sqs.sendToClient(JSON.stringify({ newPost: req.body })))
-    // .tap(() => sqs.sendToReservations(JSON.stringify({ newPost: req.body })))
+    .tap(() => sqs.sendToClient(JSON.stringify({ newPost: req.body })))
+    .tap(() => sqs.sendToReservations(JSON.stringify({ newPost: req.body })))
     .then(data => res.status(200).send(data))
     .catch(err => res.status(500).send(err))
 ));
@@ -30,8 +31,8 @@ app.get('/homes', (req, res) => (
 app.post('/homes', (req, res) => (
   // Note, this object syntax isn't supported for batch inserting!
   sql.queryAsync('INSERT INTO homes SET ?', [req.body])
-    // .tap(() => sqs.sendToClient(JSON.stringify({ newPost: req.body })))
-    // .tap(() => sqs.sendToReservations(JSON.stringify({ newPost: req.body })))
+    .tap(() => sqs.sendToClient(JSON.stringify({ newPost: req.body })))
+    .tap(() => sqs.sendToReservations(JSON.stringify({ newPost: req.body })))
     .then(() => res.status(201).send(req.body))
     .catch(err => res.status(500).send(err))
 ));
@@ -40,8 +41,8 @@ app.patch('/homes', (req, res) => {
   const updateData = Object.assign({}, req.body);
   delete updateData.id;
   return sql.queryAsync('UPDATE homes SET ? WHERE id = ?', [updateData, req.body.id])
-    // .tap(() => sqs.sendToClient(JSON.stringify({ update: req.body })))
-    // .tap(() => sqs.sendToReservations(JSON.stringify({ update: req.body })))
+    .tap(() => sqs.sendToClient(JSON.stringify({ update: req.body })))
+    .tap(() => sqs.sendToReservations(JSON.stringify({ update: req.body })))
     .then(() => res.status(201).send(req.body))
     .catch(err => res.status(500).send(err));
 });
